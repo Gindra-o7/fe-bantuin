af83fe0b4a1d4fc64a341c1a2fbeae0630ca5415
hzqula <ilooqstrasi@gmail.com>
2025-11-18 12:54:56 +0700

    feat: add new host for images

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,9 @@ const nextConfig: NextConfig = {
         hostname: "ytrxhrltkqztkcnqlcrz.supabase.co",
       },
       { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "tse2.mm.bing.net" },
+      { protocol: "https", hostname: "tse1.mm.bing.net" },
+      { protocol: "https", hostname: "plus.unsplash.com" },
     ],
   },
 };
